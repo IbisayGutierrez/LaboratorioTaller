ad44fec2097974529f27186ed377d4cc2009a5f5
ErickRoAl <errodriguezal@est.utn.ac.cr>
2024-08-13 15:50:30 -0600

    Se crea ClaseReparaciones
    Se crea claseReparacion con ColaEspera y RapairHistory

---
+++ ColaDeEspera.java
@@ -0,0 +1,47 @@
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Student
+ */
+
+public class ColaDeEspera {
+    private Queue<Reparacion> colaDeEspera;
+
+    public ColaDeEspera() {
+        colaDeEspera = new LinkedList<>();
+    }
+
+    // Agrega una nueva reparación a la cola de espera
+    public void addRepair(Reparacion reparacion) {
+        if (reparacion != null) {
+            colaDeEspera.add(reparacion);
+        } else {
+            System.out.println("Invalid repair.");
+        }
+    }
+
+    // Procesa la reparación en la cabeza de la cola
+    public Reparacion procesarReparacion() {
+        Reparacion reparacion = colaDeEspera.poll();
+        if (reparacion == null) {
+            System.out.println("There are no more repairs to process.");
+        }
+        return reparacion;
+    }
+
+    // Devuelve la cola de espera
+    public Queue<Reparacion> getColaDeEspera() {
+        return colaDeEspera;
+    }
+
+    
+}
