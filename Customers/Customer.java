3da5b0d7b59689ebc3b8718b24adb5694b182abd
jefferson <jefrodriguezgon@est.utn.ac.cr>
2024-08-13 14:28:08 -0600

    Se actualizo la clase Customers

--- Customers/Customer.java
+++ Customers/Customer.java
@@ -9,5 +9,51 @@ package Customers;
  * @author 9552
  */
 public class Customer {
+    private String Id;
+    private String Name;
+    private String PhoneNumber;
+    private String Direction;
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getNombre() {
+        return Name;
+    }
+
+    public String getTelefono() {
+        return PhoneNumber;
+    }
+
+    public String getDireccion() {
+        return Direction;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.PhoneNumber = Telefono;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direction = Direccion;
+    }
+
+    public Customer(String Id, String Name, String PhoneNumber, String Direction) {
+        this.Id = Id;
+        this.Name = Name;
+        this.PhoneNumber = PhoneNumber;
+        this.Direction = Direction;
+    }
+
+    public Customer() {
+        this("","","","");
+    }
     
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "Id=" + Id + ", Nombre=" + Name + ", Telefono=" + PhoneNumber + ", Direccion=" + Direction + '}';
+    }
+        
 }
+
